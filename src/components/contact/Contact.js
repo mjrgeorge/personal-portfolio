44c4b16abfc80acce0d1b26a4e01d4cd3399ca84
mjrgeorge <mjrgeorge@gmail.com>
2020-11-01 09:22:35 +0600

    contact component updated

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import NavBar from '../home/navBar/NavBar';
-import message from '../../images/message.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeOpen, faPhoneAlt, faSearchLocation } from '@fortawesome/free-solid-svg-icons';
 
 export default function Contact() {
 
@@ -20,28 +21,44 @@ export default function Contact() {
     return (
         <>
             <NavBar />
-            <div className="row m-5">
+            <div className="row m-4">
                 <div className="col-md-6">
-                    <img className="p-5 img-fluid" src={message} alt="message" required />
+                    <div className="text-center text-danger font-weight-bold h5 contact-form shadow-lg m-3 bg-white rounded">
+                        <div className="shadow p-4">
+                            <big><p><FontAwesomeIcon icon={faPhoneAlt} /></p></big>
+                            <p>Call Me On</p>
+                            <p><a className="ml-3" href="tel:+8801841503316">+8801841503316</a></p>
+                        </div>
+                        <div className="shadow p-4">
+                            <big><p><FontAwesomeIcon icon={faEnvelopeOpen} /></p></big>
+                            <p>Email</p>
+                            <p><a href="mailto:mjrgeorge@gmail.com">mjrgeorge@gmail.com</a></p>
+                        </div>
+                        <div className="shadow p-4">
+                            <big><p><FontAwesomeIcon icon={faSearchLocation} /></p></big>
+                            <p>Location</p>
+                            <a href="#">Dhaka, Bangladesh</a>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-md-6">
                     <form className="contact-form shadow-lg p-5 m-3 bg-white rounded" onSubmit={sendEmail}>
-                        <div class="form-group">
+                        <div class="form-group pb-1">
                             <input className="form-control" type="hidden" name="contact_number" required />
                         </div>
-                        <div class="form-group">
+                        <div class="form-group pb-1">
                             <label>Name</label>
                             <input className="form-control" type="text" name="user_name" required />
                         </div>
-                        <div class="form-group">
+                        <div class="form-group pb-1">
                             <label>Email</label>
                             <input className="form-control" type="email" name="user_email" required />
                         </div>
-                        <div class="form-group">
+                        <div class="form-group pb-1">
                             <label>Message</label>
                             <textarea className="form-control" name="message" cols="30" rows="3" required />
                         </div>
-                        <div class="form-group">
+                        <div class="form-group pb-1">
                             <input className="btn btn-info btn-block" type="submit" value="Send" required />
                         </div>
                     </form>
