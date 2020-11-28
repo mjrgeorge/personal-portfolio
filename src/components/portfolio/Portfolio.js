e932e6b3db98f08f0cb6d54108e1945624055314
mjrgeorge <mjrgeorge@gmail.com>
2020-11-28 20:43:22 +0600

    Portfolio more project added

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,6 +4,13 @@ import project2 from '../../images/projects/project2.png';
 import project3 from '../../images/projects/project3.png';
 import project4 from '../../images/projects/project4.png';
 import project5 from '../../images/projects/project5.png';
+import project6 from '../../images/projects/project6.png';
+import project7 from '../../images/projects/project7.png';
+import project8 from '../../images/projects/project8.png';
+import project9 from '../../images/projects/project9.png';
+import project10 from '../../images/projects/project10.png';
+import project11 from '../../images/projects/project11.png';
+import project12 from '../../images/projects/project12.png';
 
 const Portfolio = () => {
     return (
@@ -100,6 +107,116 @@ const Portfolio = () => {
                     <div className="card-footer d-flex justify-content-between">
                         <a href="https://athena-design-react.netlify.app/" className="card-link">Live</a>
                         <a href="https://github.com/faisalcep/athena-design" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project6} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Travel Guru</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#React</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Firebase</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://travel-guru-85690.web.app/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/travel-guru" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project7} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Hard Rock</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#JavaScript</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/hard-rock/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/hard-rock" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project8} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Shopping Cart</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#JavaScript</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/shopping-cart/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/shopping-cart" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project9} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">George Bank</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#JavaScript</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/george_bank/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/george_bank" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project10} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Leader Board Website</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/leader_board_website/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/leader_board_website" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project11} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Hot Gadgets Website</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/hot_gadgets_website/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/hot_gadgets_website" className="card-link">Github</a>
+                    </div>
+                </div>
+                <div className="card mx-auto mt-5">
+                    <img src={project12} className="card-img-top" alt="work" />
+                    <div className="card-body">
+                        <h5 className="card-title">Panda Commerce Website</h5>
+                        <div className="d-flex flex-wrap">
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#HTML</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#CSS</span>
+                            <span className="bg-dark px-2 m-2 text-white rounded-pill">#Bootstrap</span>
+                        </div>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <a href="https://mjrgeorge.github.io/panda-commerce/" className="card-link">Live</a>
+                        <a href="https://github.com/mjrgeorge/panda-commerce" className="card-link">Github</a>
                     </div>
                 </div>
             </div>
