ce098a9b886e96a8bc547ffa7cf43d50c5de32cf
Jubayer Rahman <mjrgeorge@gmail.com>
2024-06-30 23:09:15 +0600

    my data  updated

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -10,14 +10,14 @@ const Header = () => {
                 <div className="col-lg-6 p-4">
                     <h2 className="display-4 text-dark">Hello I'm</h2>
                     <h1 className="text-warning">Jubayer Rahman</h1>
-                    <h4 style={{color:"#FA6957"}}>
+                    <h4 style={{ color: "#FA6957" }}>
                         <Typical
-                            steps={['A Programmer', 2000, 'A Front End Developer', 2000, 'A MERN Stack Developer', 2000]}
+                            steps={['A Programmer', 2000, 'A Front End Developer', 2000, 'A Backend Developer', 2000, 'A Full Stack Developer', 2000]}
                             loop={Infinity}
                             wrapper="b"
                         />
                     </h4>
-                    <p className="text-capitalize text-justify text-muted h5">I want to built-up my career as a Web Developer. I love to code modern and responsive layouts for web application. My core skill is based on JavaScript and I love to do most interesting things using React.Js And Node.Js. I think I have enough Knowledge In The HTML, CSS, SASS, Bootstrap, JavaScript, ES6, React.Js, React-Router, React-Bootstrap, Material-UI, Firebase, Netlify And Also Comfortable With Node.JS, Express.JS, MongoDB. I try to improve Myself in  Every moment.</p>
+                    <p className="text-capitalize text-justify text-muted h5">As a passionate and dedicated web developer, I specialize in creating modern and responsive web applications. With a core skill set centered around JavaScript, I thrive on developing innovative solutions using React.js and Node.js. My journey in web development is driven by a continuous desire to improve and optimize my workflow. I am always eager to learn new technologies and apply them to real-world projects. Whether it's building a sleek front-end interface or crafting a robust back-end system, I approach each project with enthusiasm and a commitment to excellence.</p>
                     <a className="btn btn-outline-info font-weight-bold mt-1" href="https://docs.google.com/document/d/1xKf-0kOG1sd2OAGuCKZrF2xC42QM9pfbcihGcojTKyA/export?format=pdf" target="_blank"><big>Download Resume</big></a>
                     <Link className="btn btn-outline-info font-weight-bold ml-2 mt-1" to="/contact"><big>Hire Me</big></Link>
                 </div>
